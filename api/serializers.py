c991b6b338b1e7f971353ba69ae92e5a9e31a6db
p1chkn <p7chkn@yandex.ru>
2020-12-17 16:11:44 +0700

    start date validation

--- api/serializers.py
+++ api/serializers.py
@@ -8,3 +8,8 @@ class BookingListSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('workplace', 'datetime_from', 'datetime_to', 'author')
         model = BookingList
+    
+    def validate(self, attrs):
+        if attrs['datetime_from'] > attrs['datetime_to']:
+            raise serializers.ValidationError('Date_from more than date_to')
+        return attrs
