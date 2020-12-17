4bb4bc421fda8bed07b0d32d4bc17f9569ac60dc
p1chkn <p7chkn@yandex.ru>
2020-12-17 14:02:31 +0700

    starting models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Workplace(models.Model):
+    cabinet_number = models.IntegerField()
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return str(self.cabinet_number)
+
+
+class Booking(models.Model):
+    workplace = models.ForeignKey(Workplace, on_delete=models.CASCADE)
+    datetime_from = models.DateTimeField()
+    datetime_to = models.DateTimeField()
+
+    def __str__(self):
+        return self.workplace
