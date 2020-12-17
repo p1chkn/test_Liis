72f3e7981b739f26181ba34879d4aafab21f6343
p1chkn <p7chkn@yandex.ru>
2020-12-17 16:44:52 +0700

    add date restriction

--- api/serializers.py
+++ api/serializers.py
@@ -1,3 +1,5 @@
+import datetime
+
 from rest_framework import serializers
 
 from .models import BookingList
@@ -8,8 +10,30 @@ class BookingListSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('workplace', 'datetime_from', 'datetime_to', 'author')
         model = BookingList
-    
+
     def validate(self, attrs):
-        if attrs['datetime_from'] > attrs['datetime_to']:
+        date_from = attrs['datetime_from']
+        date_to = attrs['datetime_to']
+        if date_from > date_to:
             raise serializers.ValidationError('Date_from more than date_to')
+        queryset = BookingList.objects.filter(workplace=attrs['workplace'])
+        time_list = []
+        for item in queryset:
+            temp = [item.datetime_from, item.datetime_to]
+            time_list.append(temp)
+        time_list.sort()
+        for i in range(len(time_list)):
+            if date_to < time_list[i][0]:
+                continue
+            elif date_from > time_list[i][1]:
+                continue
+            elif date_from > time_list[i][0] and date_from < time_list[i][1]:
+                raise serializers.ValidationError(
+                            'This date is booked already!')
+            elif date_to > time_list[i][0] and date_to < time_list[i][1]:
+                raise serializers.ValidationError(
+                            'This date is booked already!')
+            elif date_to > time_list[i][1] and date_from < time_list[i][0]:
+                raise serializers.ValidationError(
+                            'This date is booked already!')
         return attrs
